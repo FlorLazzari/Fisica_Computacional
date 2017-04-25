fed571ef29fa904637a7731325ab6d857b4ce184
martin <martincarusso@gmail.com>
2017-04-25 19:12:19 -0300

    hecho el promediado en la masa

--- percolacion/src/Masa.c
+++ percolacion/src/Masa.c
@@ -4,28 +4,55 @@
 #include "funciones_percolacion.c"
 
 int main(){
-	int L;
+	int L = 4;
 	float p_c = 0.6; // aca poner la que corresponda
 	srand(time(NULL));
+	int i;
+	int n_iteraciones = 100; 
+	float array[n_iteraciones];
+	int m;
 
-	for (L=4; L<256; L = L*2){ // itero para cada tamaño
+	
 
-			int red[L*L];
-			//int tag[L*L];
+	for (L; L<512; L = L*2){ // itero para cada tamaño
 
-			llenar(red, L, p_c); //inicializo red
+		int red[L*L];
+		//int tag[L*L];
+		float m_prom = 0;
+
+		for (i=0; i<n_iteraciones ; i++){
+
+			 //inicializo red
 			//print_red(n, red);
-			hoshen(red, L); //hago HK en la red
+			//hago HK en la red
 			//print_red(n, red);
 			//tags(L, red, tag); //obtengo tamaños de los clusters
+			//printf("Empieza el loop\n");
+			while (true){
 
-			if (percola(L, red)){ //calcula la Masa
-				printf("La Masa del cluster percolante para L=%i es %i \n", L, masa(L, red));
+				llenar(red, L, p_c);
+				hoshen(red, L); 
+
+				if(percola(L, red)){ //calcula la Masa
+					m = masa(L, red);
+					array[i] = (float)m;	
+					if (i % 10 == 0){
+						printf("La Masa del cluster percolante para L=%i es %i \n", L, m );
+					}
+					break;
+				}
+				else{
+					continue;
+				}
 			}
+			//printf("termina el loop\n");
+			
+		}		
 
+	m_prom = promedio(L, array);			
+	printf("la masa promedio para L = %i es %f \n", L, m_prom);
 
-	}
+	}	
 
-
-	return 0;
+return 0;
 }
