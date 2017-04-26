ccbe236b101dafe567fc20184ec66af999003492
martin <martincarusso@gmail.com>
2017-04-26 20:10:42 -0300

    hechos los datos de ns

--- percolacion/src/dist_ns.c
+++ percolacion/src/dist_ns.c
@@ -7,11 +7,11 @@ int main(){
 
 	int L;
 	float p;
-	int rango_p = 10;
+	int rango_p = 100;
 	int i;
 	int s;
 	int j;
-	int n_prom = 10;
+	int n_prom = 500;
 	srand(time(NULL));
 
 	FILE *fdat;
@@ -19,7 +19,7 @@ int main(){
 	fprintf(fdat, "L	p	s	n(s)\n");
 
 
-	for (L=4; L<256; L=L*2){
+	for (L=4; L<128; L=L*2){
 
 		float array_ns[n_prom];
 
@@ -27,18 +27,18 @@ int main(){
 		float n_s[L*L];
 		printf("\n ~~~~~~~~ L = %i ~~~~~~~~\n", L);
 
-		for (i = 0; i < rango_p; i++){
+		for (i = 1; i < rango_p; i++){
 			p = (float)i/rango_p;
 			printf("\n ~~~~~~~~ p = %f ~~~~~~~~\n", p);
 
-			for (s = 0; s<L*L; s++){
+			for (s = 0; s<15; s++){
 
 					for(j = 0; j < n_prom; j++){
 						llenar(red, L, p);
 						hoshen(red, L);
 
 						array_ns[j] = ns_s(L, red, s);
-						printf("El array_ns de j = %i es array_ns = %f \n", j, array_ns[j]);
+						printf("Para L=%i, p=%f el array_ns de j = %i es array_ns = %f \n", L, p, j, array_ns[j]);
 					}
 
 					n_s[s] = promedio(n_prom, array_ns);
