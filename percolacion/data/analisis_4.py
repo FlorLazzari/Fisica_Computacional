e888e601308e302a28f66aa274b4359e26a9374f
Santi <aguilerasanti@hotmail.com>
2017-05-01 19:02:03 -0300

    terminado analisis_4

--- percolacion/data/analisis_4.py
+++ percolacion/data/analisis_4.py
@@ -8,41 +8,9 @@ import matplotlib.pyplot as plt
 
 datos_ns = np.loadtxt("datos dist_ns_posta.csv", delimiter = ',', skiprows=1)
 
-pc = 0.59
-
-p1 = np.zeros(99)
-ns1 = np.zeros(99)
-s1 = np.zeros(99)
-for i in range(99):
-    p1[i] = datos_ns[5941+15*i, 1]
-    ns1[i] = datos_ns[5941+15*i, 3]
-    s1[i] = datos_ns[5941+15*i, 2]
-
-tau = 1.64
-f1 = ns1/(0.01678) # es el ns de pc para s=1
-z1 = (s1**(0.39))*(p1-pc)
-
-plt.plot(z1, f1)
-# plt.show()
-
-
-p2 = np.zeros(99)
-ns2 = np.zeros(99)
-s2 = np.zeros(99)
-for i in range(99):
-    p2[i] = datos_ns[5942+15*i, 1]
-    ns2[i] = datos_ns[5942+15*i, 3]
-    s2[i] = datos_ns[5942+15*i, 2]
-
-
-f2 = ns2/(0.00351) # es el ns de pc para s igual a dos
-
-z2 = (s2**(0.39))*(p2-pc)
-
-plt.plot(z2, f2)
-# plt.show()
-
-################################################################################
+pc = 0.58
+tau = 1.71
+sigma = 0.39
 
 p = np.zeros((14,99))
 ns = np.zeros((14,99))
@@ -53,20 +21,39 @@ z = np.zeros((14,99))
 ns_c = np.zeros((14))
 
 for i in range(14):
-    ns_c[i] = datos_ns[6811+i, 3]
+    # ns_c[i] = datos_ns[6811+i, 3]
+    ns_c[i] = datos_ns[6796+i, 3]
 
-sigma = 0.39
+p = np.arange(0.01,1,0.01)
+s = np.arange(1,15)
 
+plt.figure()
 for i in range(14):
     for j in range(99):
-        p[i,j] = datos_ns[5941+i+15*j, 1]
         ns[i,j] = datos_ns[5941+i+15*j, 3]
-        # s[i,j] = datos_ns[5941+i+15*j, 2]
-        s = np.arange(1,15)
         f[i,j] = ns[i,j]/ns_c[i]
-        z[i,j] = (s[i]**(sigma))*(p[i,j]-pc)
+        z[i,j] = (s[i]**(sigma))*(p[j]-pc)
     plt.plot(z[i,:], f[i,:], label = "s = {}".format(s[i]))
     plt.grid(True)
+    plt.xlabel(r'$s^{\sigma} \epsilon$',fontsize = 15)
+    plt.ylabel(r'$f(z)$',fontsize = 15)
+    plt.title('Funcion de Scaling') #falta la tilde
+    plt.legend(fontsize=18, loc='best')
+
+
+################################################################################
+
+plt.figure()
+for i in range(14):
+    for j in range(99):
+        ns[i,j] = datos_ns[5941+i+15*j, 3]
+        f[i,j] = ns[i,j]/(s[i]**(-tau))
+        z[i,j] = (s[i]**(sigma))*(p[j]-pc)
+    plt.plot(z[i,:], f[i,:], label = "s = {}".format(s[i]))
+    plt.grid(True)
+    plt.xlabel(r'$s^{\sigma} \epsilon$',fontsize = 15)
+    plt.ylabel(r'$f(z)$',fontsize = 15)
+    plt.title('Funcion de Scaling') #falta la tilde
     plt.legend(fontsize=18, loc='best')
 
 
